08eb22a455e5c0398b065f65e5f77757c735fc93
Ccondir <87036822+Ccondir@users.noreply.github.com>
2021-10-19 16:43:58 +0200

    Add files via upload

---
+++ gejm2.py
@@ -0,0 +1,110 @@
+import time
+
+a = input("chceš gejmit gejmu?: ")
+
+if a == ("ano"):
+ print("hell yeah de se gejmit")
+time.sleep(2)
+name = input("jak se chceš jmenovat?: ")
+time.sleep(1)
+print("hmmmm takže Andřej, jak zvláštní")
+time.sleep(2)
+b = input("kam chceš? do města, do lesa, na kebab? (do města/do lesa/na kebab): ")
+if b == ("do města"):
+    print("to město we go")
+    time.sleep(3)
+    print("dorazil jsi do města jménem Mám Přírodopis ale místo toho dělám tuhle špatnou hru zkráceně PALMTDTŠH") #name making 100
+    time.sleep(3)
+    c = input("deš dovnitř nebo tam budeš stát jako retard? (do města/stát): ")
+    if c ==("do města"):
+        print("úspěšně jsi šel do města")
+        time.sleep(3)
+        print("vidíš spoustu liďí máš chuť vytáhnout 9mm vulcan 304, 5hlavňový rotáční kulomet a liďi rychle oddělat")
+        time.sleep(5)
+        d = input("chceš vytáhnout 9mm vulcan 304 5hlavňový rotáční kulomet a uďelat z lidí ementál? (ano/ne): ")
+        if d ==("ano"):
+            print("vytáhl jsi 9mm vulcan304 5hlavňový rotáční kulomet a začal střílt")
+            time.sleep(5)
+            print("úspěšně jsi všechny zabil")
+            time.sleep(2)
+            print("konec")
+            time.sleep(30)
+        else:
+            print("sračko...")
+        time.sleep(30)
+    elif c ==("stát"):
+        print("stojíš tam furt jako retard...")
+        time.sleep(60)
+        print("furt stojíš")
+        time.sleep(120)
+        print("čeho jsi tímhle dosáhl?")
+        time.sleep(5)
+elif b == ("do lesa"):
+    print("do lesa we go")
+    time.sleep(3)
+    print("jsi u temnýho lesa a vypadá jako les")
+    time.sleep(5)
+    d = input("můžeš jít sbírat houby, jen tak se projít nebo se otočit a jít jinam. kam deš? (sbírat houby/jen tak se projít/otočit se a jít jinam): ")
+    if d ==("sbírat houby"):
+        print("máš fakt nudenj život ale našel jsi houby... gratuluji")
+        time.sleep(5)
+        e = input("sežereš je?: ")
+        if e ==("ano"):
+            print("byli to magické houbičky... pokud víš co myslím")
+            time.sleep
+            print("během toho co jsi byl na tripu tak si zaběhl do města a všechny vystřílel 9mm 5hlavňovým vulcan 304 kulometem")
+            time.sleep(10)
+            print("gratuluji došel jsi do konce")
+            time.sleep(60)
+        else:
+            print("sežral si je stejně protože na ních sež závyslej")
+            time.sleep(5)
+            print("během toho co jsi byl na tripu tak si zaběhl do města a všechny vystřílel 9mm 5hlavňovým vulcan 304 rotáčním kulometem")
+            time.sleep(10)
+            print("gratuluji došel jsi do konce")
+            time.sleep(60)
+    elif d ==("jen tak se projít"):
+        print("během prochásky se ti nic nestalo a byla tak neuspokojivá že jsi šel vystřílet město s 9mm 5hlavňovým vulcan 304 rotáčním kulometem")
+        time.sleep(20)
+        print("konec")
+        time.sleep(60)
+    elif d ==("otočit se a jít jinam"):
+        print("šel jsi do města a tam jsi všechny velmi mýrumilovně pohlaďil a poté rozsřílel 9mm 5 hlavňovým vulcan 304 rotáčním kulometem")
+        time.sleep(60)
+elif b == ("na kebab"):
+    print("to kebab we go")
+    time.sleep(3)
+    print("dorazil jsi na stánek s kebabem urostřed ničeho")
+    time.sleep(5)
+    print("stojí tam starší pán a ptá se")
+    time.sleep(3)
+    print("tak copak to bude Andřeji")
+    time.sleep(2)
+    print("zase klasika eh?")
+    time.sleep(3)
+    f = input("co na to odpovíš, jsi zaskočen žezná tvé jméno, tolik otázek ale jen jeden předraženný kebab od odpovědí (ano klasiku/kdo jsi?): ")
+    if f ==("ano klasiku"):
+        print("starý pán ti podává vydlabaný ananas a v ňem všchno to skvělé maso s omáčkou")
+        time.sleep(10)
+        print("neudržel ses zašal jsi brečet a opakovaně do starého muž střílet svým 9mm 5hlavňovým vulcan 304 rotáčním kulometem a křičel na něj: jak si tohle můžeš dovolit ty zrůdo ty jeden jeden jeden")
+        time.sleep(20)
+        print("po opakované palbě nezbyde ze staříka nic jen ten bohemprokletej kebab")
+        time.sleep(5)
+        print("konec")
+        time.sleep(90)
+    elif f ==("kdo jsi?"):
+         print("muž odpový: znáš heslo?")
+         time.sleep(3)
+         g = input("to je dobrá otázka, znáš heslo? (ne/[heslo]): ")
+         if g ==("ne"):
+             print("tak to se asy nemůžeme byvit")
+             time.sleep(3)
+             print("řekl stařík a stánek zavřel")
+             time.sleep(5)
+             print("konec")
+             time.sleep(60)
+         elif g ==("PALMTDTŠH"):
+          print("ahh tak ty jsi taky součástí")
+         time.sleep(5)
+         print("řekni majiteli hry že jsi našel tento dialog")
+         time.sleep(30)
